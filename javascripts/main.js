2bca71c1e48e70aaadefacb22ee7b81bd0031e5b
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-24 21:04:36 +0300

    second commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 console.log('This would be the main JS file.');
+
+var headUrl = 'http://ivansimeyko.github.io/lesson14/';
+var htmlList = ['simple_1.html', 'simple_2.html', 'simple_3.html'];
+
+function loadHtmlFiles(element) {
+	$.ajax({
+		url: headUrl + element,
+		success: function(data) {
+			$('.wrapper').append(data);
+			//$(document.body).append(data);
+		},
+		error: function(data) {
+		  console.error(data);
+		}
+	});
+}
+
+htmlList.forEach( loadHtmlFiles );
