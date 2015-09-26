994af5faf28d8d21ecfba57a8ebd22ea4c3c22a1
Ivan Simeyko <iv.simeyko@gmail.com>
2015-09-26 19:07:33 +0300

    add in index.html simple_2.html

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ console.log('This would be the main JS file.');
 
 
 var headUrl = 'http://ivansimeyko.github.io/Ajax_example/';
-var htmlList = ['simple_1.html',];    //'simple_2.html', 'simple_3.html'];
+var htmlList = ['simple_1.html' ,'simple_2.html', ];    //'simple_3.html'];
 
 function loadHtmlFiles(element) {
 	$.ajax({
